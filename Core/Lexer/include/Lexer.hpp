f587aa1b6c2a9f91ce3b128f901044261f28e80f
Werner SEEGERS <wseegers@c1r1s2.wethinkcode.co.za>
2019-06-26 17:05:24 +0200

    Moved grammer instruction

--- Core/Lexer/include/Lexer.hpp
+++ Core/Lexer/include/Lexer.hpp
@@ -1,36 +1,6 @@
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
-/*
-S := INSTR [SEP INSTR]* #
-
-INSTR :=
-	push VALUE
-	| pop
-	| dump
-	| assert VALUE
-	| add
-	| sub
-	| mul
-	| div
-	| mod
-	| print
-	| exit
-
-VALUE :=
-	int8(N)
-	| int16(N)
-	| int32(N)
-	| float(Z)
-	| double(Z)
-
-N := [-]?[0..9]+
-
-Z := [-]?[0..9]+.[0..9]+
-
-SEP := '\n'+
-*/
-
 #include "../../interface/ILexer.hpp"
 
 #include <regex>
